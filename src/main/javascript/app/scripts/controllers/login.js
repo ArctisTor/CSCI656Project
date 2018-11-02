3079cb5611859538d70fef3ccb7c96fdd56d5a11
cplatz <cplatz@netrist.com>
2018-11-02 15:32:28 -0400

    login and register work.

--- src/main/javascript/app/scripts/controllers/login.js
+++ src/main/javascript/app/scripts/controllers/login.js
@@ -3,26 +3,50 @@
 angular.module('chatWebApp')
     .controller('LoginCtrl', function($scope, $state, $filter, $uibModal, toastr, api) {
 
-
         $scope.user = {
             username : '',
             password: '',
+            register: false
         };
 
         $scope.login = function() {
 
+          if ($scope.user.register) {
+
             var request = {
-                userName : $scope.user.username,
-                password : $scope.user.password
+              userName : $scope.user.newUser,
+              password: $scope.user.newPass
+            };
+
+            return api.registerUser(request).then(
+              (result) => {
+               console.log(result);
+              }
+            ).catch( (err) => {
+              toastr.error('There was an error with registering the account.');
+              console.log(err);
+            })
+
+          } else {
+
+            var request = {
+              userName : $scope.user.username,
+              password : $scope.user.password
             };
 
             return api.loginUser(request).then(
               (result) => {
-                  console.log(result);
+                console.log(result);
               }).catch( (err) => {
-                toastr.error('Invalid user/password combination.');
-                console.log(err);
+              toastr.error('Invalid user/password combination.');
+              console.log(err);
             });
+
+          }
+        };
+
+        $scope.register = function() {
+          $scope.user.register = !$scope.user.register;
         };
 
     });
